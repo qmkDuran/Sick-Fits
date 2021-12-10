e4bd283492cef90485a8e9371b22645a064a28f7
Michael Duran <michaelduran@gmail.com>
2021-12-09 21:26:37 -0500

    12/9/21 commit

--- backend/mutations/checkout.ts
+++ backend/mutations/checkout.ts
@@ -1,20 +1,19 @@
-import { CartItem } from '../schemas/CartItem';
-import { User } from '../schemas/User';
+import { CartItem } from "../schemas/CartItem"
+import { User } from "../schemas/User"
 import {
   CartItemCreateInput,
   OrderCreateInput,
-} from '../.keystone/schema-types';
+} from "../.keystone/schema-types"
 
 /* eslint-disable */
-import { KeystoneContext, SessionStore } from '@keystone-next/types';
-import stripeConfig from '../lib/stripe';
+import { KeystoneContext, SessionStore } from "@keystone-next/types"
+import stripeConfig from "../lib/stripe"
 
-const graphql = String.raw;
+const graphql = String.raw
 
 interface Arguments {
   token: string
 }
-
 
 async function checkout(
   root: any,
@@ -22,9 +21,9 @@ async function checkout(
   context: KeystoneContext
 ): Promise<OrderCreateInput> {
   // 1. Make sure they are signed in
-  const userId = context.session.itemId;
-  if(!userId) {
-    throw new Error('Sorry! You must be signed in to create an order!')
+  const userId = context.session.itemId
+  if (!userId) {
+    throw new Error("Sorry! You must be signed in to create an order!")
   }
   // 1.5 Query the current user
   const user = await context.lists.User.findOne({
@@ -50,55 +49,61 @@ async function checkout(
           }
         }
       }
-    `
-  });
+    `,
+  })
   console.dir(user, { depth: null })
   // 2. calc the total price for their order
-  const cartItems = user.cart.filter(cartItem => cartItem.product);
-  const amount = cartItems.reduce(function(tally: number, cartItem: CartItemCreateInput) {
-    return tally + cartItem.quantity * cartItem.product.price;
-  }, 0);
-  console.log(amount);
+  const cartItems = user.cart.filter((cartItem) => cartItem.product)
+  const amount = cartItems.reduce(function (
+    tally: number,
+    cartItem: CartItemCreateInput
+  ) {
+    return tally + cartItem.quantity * cartItem.product.price
+  },
+  0)
+  console.log(amount)
   // 3. create the charge with the stripe library
-  const charge = await stripeConfig.paymentIntents.create({
-    amount,
-    currency: 'USD',
-    confirm: true,
-    payment_method: token,
-  }).catch(err => {
-    console.log(err);
-    throw new Error(err.message);
-  });
+  const charge = await stripeConfig.paymentIntents
+    .create({
+      amount,
+      currency: "USD",
+      confirm: true,
+      payment_method: token,
+    })
+    .catch((err) => {
+      console.log(err)
+      throw new Error(err.message)
+    })
   console.log(charge)
   // 4. Convert the cartItems to OrderItems
-  const orderItems = cartItems.map(cartItem => {
+  const orderItems = cartItems.map((cartItem) => {
     const orderItem = {
       name: cartItem.product.name,
       description: cartItem.product.description,
       price: cartItem.product.price,
       quantity: cartItem.quantity,
-      photo: { connect: { id: cartItem.product.photo.id }},
+      photo: { connect: { id: cartItem.product.photo.id } },
     }
-    return orderItem;
+    return orderItem
   })
-  console.log('gonna create the order')
+  console.log("gonna create the order")
   // 5. Create the order and return it
   const order = await context.lists.Order.createOne({
     data: {
       total: charge.amount,
       charge: charge.id,
       items: { create: orderItems },
-      user: { connect: { id: userId }}
+      user: { connect: { id: userId } },
     },
     resolveFields: false,
-  });
+  })
   // 6. Clean up any old cart item
-  const cartItemIds = user.cart.map(cartItem => cartItem.id);
-  console.log('gonna create delete cartItems')
+  const cartItemIds = user.cart.map((cartItem) => cartItem.id)
+  console.log("gonna create delete cartItems")
   await context.lists.CartItem.deleteMany({
-    ids: cartItemIds
-  });
-  return order;
+    ids: cartItemIds,
+  })
+  return order
 }
 
-export default checkout;
+export default checkout
---
+++ frontend/.next/static/webpack/webpack.73f0c2db10c6d2976062.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "8bed44390a56b55f2b9d"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay43M2YwYzJkYjEwYzZkMjk3NjA2Mi5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjhiZWQ0NDM5MGE1NmI1NWYyYjlkXCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
